188ab8dd0744ee033e9a61a267c0324830282198
eliezer <eliezer.moraes@outlook.com>
2022-03-03 15:53:57 -0300

    Implementado funcionalidades. Testes de integração. OK

--- src/main/java/com/devsuperior/bds04/entities/City.java
+++ src/main/java/com/devsuperior/bds04/entities/City.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "tb_city")
